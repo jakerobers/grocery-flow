9b39e539e45335948c1fab95b8f977894ad86cf1
Jake Robers <jake@jakerobers.com>
2025-01-05 16:50:25 -0600

    adds quantities

--- src/utils.py
+++ src/utils.py
@@ -3,29 +3,6 @@ import subprocess
 import os
 import re
 
-CLEAN_INGREDIENT_PTN = r"^(to taste|to serve|[\d]+(?:\.\d+)?(?:\s(?:diced|oz|cups?|cloves?|grams?|teaspoons?|pinches?|packages?|tablespoons?|sliced|grated|ground|melted|ounces|lb|minced|tbsp|tsp|bunch|chopped|halved|scoop|juiced|pounds|stalks?|pinch|cans?|squeezed))*)\s*"
-
-# TODO: make this configurable from the UI
-PINNED_RECIPES = ["bean_stuff.md", "mexican_quinoa.md"]
-
-
-def get_printer_name(logger=None):
-    # printer {{name}} is idle.  enabled since {{datetime}}
-    lpstat_output = subprocess.run(
-        ["lpstat", "-p"], capture_output=True, text=True
-    ).stdout
-
-    pattern = r"printer (\S+)"
-    match = re.search(pattern, lpstat_output)
-
-    if match:
-        printer_name = match.group(1)
-        logger.info(f"Will send print to {printer_name}")
-        return printer_name
-    else:
-        logger.error("No printer found.")
-        return None
-
 
 def create_latex_document(template, tex_path, meals, all_ingredients, logger):
     logger.info("Creating generated-list.tex using template.tex")
@@ -37,8 +14,13 @@ def create_latex_document(template, tex_path, meals, all_ingredients, logger):
     items = []
     for ingredient in all_ingredients:
         tags = ", ".join(ingredient["tags"])
-        quantities = ", ".join(ingredient["quantities"])
-        items.append(f"{ingredient['name']} ({quantities}) ({tags})")
+        if len(ingredient["quantities"]) > 0:
+            quantities = ", ".join(ingredient["quantities"])
+            quantities = f" ({quantities})"
+        else:
+            quantities = ""
+
+        items.append(f"{ingredient['name']}{quantities} ({tags})")
 
     data = {"items": items, "meals": rendered_meals}
     filled_template = template.render(data)
@@ -95,10 +77,9 @@ class MealBuilder:
         all_ingredients = []
         for meal in meals:
             for meal_ingredient in meal["ingredients"]:
+                quantity = None
                 if meal_ingredient["quantity"]:
                     quantity = f"{round(meal_ingredient['quantity'], 2)} {meal_ingredient['unit']}"
-                else:
-                    quantity = meal_ingredient["unit"]
 
                 found = False
                 i = 0
@@ -106,15 +87,20 @@ class MealBuilder:
                     if all_ingredients[i]["name"] == meal_ingredient["name"]:
                         found = True
                         all_ingredients[i]["tags"].append(meal["short_code"])
-                        all_ingredients[i]["quantities"].append(quantity)
+                        if quantity:
+                            all_ingredients[i]["quantities"].append(quantity)
                     i += 1
 
                 if not found:
+                    quantities = []
+                    if quantity:
+                        quantities = [quantity]
+
                     all_ingredients.append(
                         {
                             "name": meal_ingredient["name"],
                             "tags": [meal["short_code"]],
-                            "quantities": [quantity],
+                            "quantities": quantities,
                         }
                     )
 
